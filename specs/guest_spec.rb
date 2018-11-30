2d29749432552bb8346c41ad3db5e4b73561b660
Michael Irwin <mike.irwin1@gmail.com>
2018-11-30 18:35:28 +0000

    guest wallet added

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -6,14 +6,16 @@ require_relative( '../guest' )
 class GuestTest < MiniTest::Test
 
   def setup
-    @guest = Guest.new("Jeff")
+    @guest = Guest.new("Jeff", 30)
   end
 
   def test_guest_has_name
     assert_equal("Jeff", @guest.name)
   end
 
-
+  def test_guest_has_money
+    assert_equal(30, @guest.wallet)
+  end
 
 
 end
